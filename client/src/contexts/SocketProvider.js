48cbf6cb83e4d3c33c75fc6674cc262fbac7ced3
Danila Kamyshov <danila.kamyshov@mobirate.com>
2023-12-27 23:55:57 +0400

    asd

--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -11,7 +11,7 @@ export function SocketProvider({id, children}) {
     const [socket, setSocket] = useState()
 
     useEffect(() => {
-        const newSocket = io('https://gigachat-6pzo.onrender.com:10000', {query: {id}})
+        const newSocket = io('gigachat-6pzo.onrender.com:10000', {query: {id}})
         setSocket(newSocket)
 
         return () => newSocket.close()
