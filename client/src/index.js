afef4686f265718c180a18c3e87affe65ca270f8
Danila Kamyshov <danila.kamyshov@mobirate.com>
2023-12-27 21:26:13 +0400

    added some settings

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+app.use(express.static('client/build'));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(client/build, "index.html"));
+});
+
 ReactDOM.render(
     <React.StrictMode>
         <App/>
